1e18158fc69e0b45f8036c9acdcd91da70ac237a
Salma_Salah <salma.s.kabel@gmail.com>
2023-11-01 14:39:19 -0700

    task14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -10,14 +10,14 @@
 int binary_tree_balance(const binary_tree_t *tree)
 {
 	int l, r;
-	static int v = 0;
+	static int v = 1;
 
 	if (tree == NULL)
 		return (0);
 	v++;
 	l = binary_tree_balance(tree->left);
 	r = binary_tree_balance(tree->right);
-	if (v > 1)
+	if (v > 2)
 	{
 		v--;
 		if (l == 0 && r == 0)
@@ -27,6 +27,6 @@ int binary_tree_balance(const binary_tree_t *tree)
 		else
 			return (r + 1);
 	}
-	v = 0;
+	v = 1;
 	return (l - r);
 }
